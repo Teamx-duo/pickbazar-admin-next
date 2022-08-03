56432738c8b412e41a732ea65a59e2115d4f4d06
Usama Irfan <usama.irfan9797@gmail.com>
2022-08-03 20:40:18 +0500

    Changes for product edits and several small changes in products and categories

--- src/components/product/product-category-input.tsx
+++ src/components/product/product-category-input.tsx
@@ -1,9 +1,9 @@
-import SelectInput from "@components/ui/select-input";
-import Label from "@components/ui/label";
-import { Control, useFormState, useWatch } from "react-hook-form";
-import { useEffect } from "react";
-import { useCategoriesQuery } from "@data/category/use-categories.query";
-import { useTranslation } from "next-i18next";
+import SelectInput from '@components/ui/select-input';
+import Label from '@components/ui/label';
+import { Control, useFormState, useWatch } from 'react-hook-form';
+import { useEffect } from 'react';
+import { useCategoriesQuery } from '@data/category/use-categories.query';
+import { useTranslation } from 'next-i18next';
 
 interface Props {
   control: Control<any>;
@@ -11,34 +11,35 @@ interface Props {
 }
 
 const ProductCategoryInput = ({ control, setValue }: Props) => {
-  const { t } = useTranslation("common");
+  const { t } = useTranslation('common');
   const type = useWatch({
     control,
-    name: "type",
+    name: 'type',
   });
   const { dirtyFields } = useFormState({
     control,
   });
   useEffect(() => {
-    if (type?.slug && dirtyFields?.type) {
-      setValue("categories", []);
+    if (type?._id && dirtyFields?.type) {
+      setValue('categories', []);
     }
-  }, [type?.slug]);
+  }, [type?._id]);
 
   const { data, isLoading: loading } = useCategoriesQuery({
     limit: 999,
-    type: type.slug,
+    page: 1,
+    type: type._id,
   });
 
   return (
     <div className="mb-5">
-      <Label>{t("form:input-label-categories")}</Label>
+      <Label>{t('form:input-label-categories')}</Label>
       <SelectInput
         name="categories"
         isMulti
         control={control}
         getOptionLabel={(option: any) => option.name}
-        getOptionValue={(option: any) => option.id}
+        getOptionValue={(option: any) => option._id}
         // @ts-ignore
         options={data?.categories?.data}
         isLoading={loading}
