54a08fb90e1e013e5f306c9aa779b882a2594a72
Usama Irfan <usama.irfan9797@gmail.com>
2022-08-30 13:51:10 +0500

    Fixes

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,10 +1,11 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   i18n: {
-    locales: ["en", "de", "es", "ar", "he", "zh"],
-    defaultLocale: "en",
+    locales: ['en', 'de', 'es', 'ar', 'he', 'zh'],
+    defaultLocale: 'en',
     // localeDetection: false,
   },
-  localePath: path.resolve("./public/locales"),
+  localePath: path.resolve('./public/locales'),
+  react: { useSuspense: false },
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,9 +14,9 @@ export default class CustomDocument extends Document {
   render() {
     const { locale } = this.props.__NEXT_DATA__;
     const dir = locale === "ar" || locale === "he" ? "rtl" : "ltr";
-    if (process.env.NODE_ENV !== "production") {
-      i18n!.reloadResources(locale);
-    }
+    // if (process.env.NODE_ENV !== "production") {
+    //   i18n!.reloadResources(locale);
+    // }
 
     return (
       <Html>
